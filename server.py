0fd3eb7c765e16e16f0e39b8bc8daf397dbaa744
Paul De Ulloa <pauld143@gmail.com>
2023-08-05 23:46:29 -0700

    key added

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "Keep it secret, keep it safe"
 
 
 # our index route will handle rendering our form
