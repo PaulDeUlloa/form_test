dd9e6b1b518114a4052a51070033bfeb77225211
Paul De Ulloa <pauld143@gmail.com>
2023-08-05 23:44:23 -0700

    redirect practice

--- server.py
+++ server.py
@@ -13,8 +13,17 @@ def index():
 def create_user():
     print("Succesfully recieved POST info")
     print(request.form)
-    return redirect("/")
+    name = request.form["name"]
+    email = request.form["email"]
+    return redirect("/show")
+
+
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template("show.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="localhost", port=8007)
